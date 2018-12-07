eeafef4043d0eb775c2aa6c279035df9c899ec65
Leo Mancini <mancini@leo.gd>
2018-12-07 11:56:53 -0800

    Adding file to change data visibility

--- helpers/toggle-data-visibility-config.php
+++ helpers/toggle-data-visibility-config.php
@@ -2,7 +2,7 @@
 	if($_GET["visibility"] == "show") {
 		putenv("SHOW_RATINGS=true");
 		putenv("SHOW_DATA=true");
-	} elseif($_GET["visibility"] == "false") {
+	} elseif($_GET["visibility"] == "hide") {
 		putenv("SHOW_RATINGS=false");
 		putenv("SHOW_DATA=false");
 	}
