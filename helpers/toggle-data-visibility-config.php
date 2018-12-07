3cc8a906d67d65a76b9df92bf86f6a7f86e17327
Leo Mancini <mancini@leo.gd>
2018-12-07 11:59:28 -0800

    Removing file to change data visibility

--- helpers/toggle-data-visibility-config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if($_GET["visibility"] == "show") {
-		putenv("SHOW_RATINGS=true");
-		putenv("SHOW_DATA=true");
-	} elseif($_GET["visibility"] == "hide") {
-		putenv("SHOW_RATINGS=false");
-		putenv("SHOW_DATA=false");
-	}
-?>
